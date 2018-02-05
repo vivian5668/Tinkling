1bc84483026ba8a899ab38be25b12e9d7e2a9bb1
Chelsea Zhu <qianwei5668@gmail.com>
2018-02-05 11:36:21 -0800

    checkWin function complete

--- js/app.js
+++ js/app.js
@@ -7,17 +7,53 @@ var level = 1;
 
 var giveNotes = function () {
 	var rand = Math.floor(Math.random() * 8);
-	var notes = allNotes;
+	notesGiven = allNotes;
 	for (var i = 0; i <= level + 3; i++) {
-		notes.splice(rand - i, 1);
+		notesGiven.splice(rand - i, 1);
 	}
-	console.log(notes)
+}
+
+
+var startGame = function () {
+	giveNotes();
+	console.log('notesGiven: ' + notesGiven)
 }
 
 var dotClicked = function () {
 	//dot jump
 	notesPlayed.push(this.id);
-	console.log(notesPlayed)
+	console.log('notesPlayed: ' + notesPlayed);
+	checkNote();
+}
+
+var checkNote = function () {
+	//check  for match once a note is played, stop the game if there is an un-match
+	if (notesGiven.length > notesPlayed.length) {	
+		for (var i = 0; i < notesPlayed.length; i++) {
+			if (notesPlayed[i] !== notesGiven[i]) {
+				fail();
+				console.log('fail');
+				return;
+			} 
+		} 
+	}
+	//if all previous tests pass, check for final match
+	if (notesGiven.length === notesPlayed.length) {
+		for (var i = 0; i < notesGiven.length; i++) {
+			if (notesPlayed[i] === notesGiven[i]) {
+				win();
+				console.log('win');
+		} 
+	  } 
+	}
+}
+
+var fail = function () {
+
+}
+
+var win = function () {
+
 }
 
 
@@ -31,6 +67,7 @@ $(document).ready(function() {
 	$('#n6').on('click', dotClicked);
 	$('#n7').on('click', dotClicked);
 	$('#n8').on('click', dotClicked);
+	$('#start').on('click', startGame);
 
 
 })
