2f85882b5d18e7d1bc8d0dc5214dc37a6ba1d9b9
Chelsea Zhu <qianwei5668@gmail.com>
2018-02-12 08:52:57 -0800

    cleaned comments

--- js/app.js
+++ js/app.js
@@ -27,16 +27,12 @@ var makeTimeoutFunction = function(id) {
 
 var giveNotes = function (levelNum) {
 	var rand;
-	var newArray = Array.from(allNotes); //initially has 8 elements
-	//randomly delete one, store in temp , now 7 left
-	//add temp to notesGiven, then repeat
+	var newArray = Array.from(allNotes); 
 	for (var i = 0; i < 2 + levelNum; i++) {
 		rand = Math.floor(Math.random()*8);
 		temp = newArray.splice(rand-i, 1);
 		notesGiven.push(temp);
 	}
-
-	console.log('notesGiven: ' + notesGiven);
 
 	time = 900;
 
@@ -52,15 +48,12 @@ var clearAnimationClass = function () {
 	for (var i = 0; i < 9; i++) {
 		$('#n' + i).removeClass('dotjump');
 	}
-	// console.log($('#n1').classList)
 }
 
 
 
 var dotClicked = function () {
-	//dot jump
 	notesPlayed.push(this.id);
-	console.log('notesPlayed: ' + notesPlayed);
 	document.getElementById('s' + this.id).play(); //to grab audio id
 	this.classList.add('dotjump');
 	//every time a note is played, check if the note is correct. 
@@ -88,10 +81,7 @@ var checkNote = function () {
 	}
 		if (matchCount === level + 2) {
 			win();
-			console.log('win')
-			// winNum ++;
-		}	
-	  
+		}		  
 	}
 }
 
@@ -116,24 +106,17 @@ var fail = function () {
 	}, 1000)
 	$('#catsad').remove(); // remove gif and re-add to make gif start at beginning each time
 	$('#catimg').append('<img class="responsive-img" id="catsad" src="img/sad.gif">'); // reload it
-	
-	console.log('fail')
-	// $('#catsad').addClass('hide');
-
 
 	for (var i = 0; i <= allNotes.length; i++) {
 			$('#' + allNotes[i]).off('click', dotClicked);
 		}
 
 	setTimeout(function() {
-		console.log('here!')
 		$('#catsad').remove();
 		$('#start').removeClass('disabled');
 		$('#start').addClass('pulse');
 		$('#start').removeClass('hide');
-		// $('#next').removeClass('disabled');
-		$('#next').addClass('hide');
-		
+		$('#next').addClass('hide');	
 	},5200)
 }
 
@@ -151,7 +134,6 @@ var win = function () {
 			$('#' + allNotes[i]).off('click', dotClicked);
 		}
 
-	console.log('winNum: ' + winNum)
 	if (winNum < totalLevel) {
 		$('#start').addClass('hide');
 		$('#next').removeClass('hide');
@@ -188,7 +170,6 @@ var win = function () {
 			$('#start').removeClass('hide');
 			$('#next').addClass('hide');
 		},3000)
-
 	}
 }
 
@@ -199,9 +180,6 @@ var startGame = function () {
 	$('#start').removeClass('pulse')
 	clearAnimationClass();
 	giveNotes(level);
-
-
-	
 }
 
 var addEventListnerToDot = function () {
